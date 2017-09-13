d956a16f5f430013b5097618033915ac9d150817
JuanReynaldoEscobar <jrescobarmiron@gmail.com>
2017-09-13 12:14:01 -0500

    configuracion

---
+++ utilities/src/main/java/com/totalplay/utilities/Persona.java
@@ -0,0 +1,13 @@
+package com.totalplay.utilities;
+
+/**
+ * Created by NS-TOTALPLAY on 13/09/2017.
+ */
+
+public class Persona {
+    public String nombre;
+
+    public String edad;
+
+    public String sexo;
+}
